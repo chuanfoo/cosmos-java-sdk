0445d54495032467b28ec219c3eb91266f69afbd
chuanfoo <xcf726@126.com>
2021-08-19 18:05:45 +0800

    Update Constants.java
    修改为新的接口

--- src/main/java/io/cosmos/common/Constants.java
+++ src/main/java/io/cosmos/common/Constants.java
@@ -2,7 +2,7 @@ package io.cosmos.common;
 
 
 public class Constants {
-    public static String COSMOS_ACCOUNT_URL_PATH = "/auth/accounts/";
+    public static String COSMOS_ACCOUNT_URL_PATH = "/auth/v1beta1/accounts/";
     public static String COSMOS_TRANSACTION_URL_PATH = "/txs";
 }
 
